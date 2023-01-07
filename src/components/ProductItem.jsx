136a7a4db5d0f70ccdd67a20713d7ab9d5b05b46
Alexander <ablancoa95@gmail.com>
2023-01-07 15:56:15 -0300

    Se agrego el useState del menu

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -4,6 +4,13 @@ import '@styles/ProductItem.scss';
 import addToCart from "@icons/bt_add_to_cart.svg"
 
 const ProductItem = () => {
+
+	const [cart, setCart] = React.useState([]);
+
+	function handleCart() {
+		setCart([]);
+	}
+
 	return (
 		<div className="ProductItem">
 			<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
@@ -12,10 +19,11 @@ const ProductItem = () => {
 					<p>$120,00</p>
 					<p>Bike</p>
 				</div>
-				<figure>
+				<figure onClick={handleCart}>
 					<img src={addToCart} alt="" />
 				</figure>
 			</div>
+
 		</div>
 	);
 }
